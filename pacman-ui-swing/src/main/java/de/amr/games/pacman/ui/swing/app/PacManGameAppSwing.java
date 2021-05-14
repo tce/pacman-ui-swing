7056c27d4e029abb817bc6608e2160a1496e0a2b
Armin Reichert <armin.reichert@web.de>
2021-05-14 08:50:34 +0200

    rename

--- pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/app/PacManGameAppSwing.java
+++ pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/app/PacManGameAppSwing.java
@@ -45,7 +45,7 @@ public class PacManGameAppSwing extends PacManGameController {
 	public static void main(String[] args) {
 		Options options = new Options(args);
 		PacManGameAppSwing app = new PacManGameAppSwing();
-		app.play(options.gameVariant);
+		app.selectGame(options.gameVariant);
 		invokeLater(() -> {
 			GameLoop gameLoop = new GameLoop(app);
 			app.setUI(new PacManGameUI_Swing(gameLoop, app, options.height));
