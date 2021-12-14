de3cab4a0d2c82f9226df17119fea3fdb5f89de3
Armin Reichert <armin.reichert@web.de>
2021-12-14 11:28:57 +0100

    adapted

--- pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/PacManGameUI_Swing.java
+++ pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/PacManGameUI_Swing.java
@@ -329,7 +329,7 @@ public class PacManGameUI_Swing implements PacManGameUI {
 		}
 
 		case KeyEvent.VK_L:
-			gameController.game().changeLivesBy(3);
+			gameController.game().lives += 3;
 			break;
 
 		case KeyEvent.VK_N:
