696d54063df0fb93473409f4bcc7c30b101bae34
Armin Reichert <armin.reichert@web.de>
2022-05-22 20:41:15 +0200

    refac

--- pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/scenes/pacman/PacMan_IntroScene.java
+++ pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/scenes/pacman/PacMan_IntroScene.java
@@ -33,8 +33,8 @@ import java.awt.RenderingHints;
 import java.util.stream.Stream;
 
 import de.amr.games.pacman.controller.GameController;
+import de.amr.games.pacman.controller.pacman.IntroContext.GhostPortrait;
 import de.amr.games.pacman.controller.pacman.IntroController;
-import de.amr.games.pacman.controller.pacman.IntroController.GhostPortrait;
 import de.amr.games.pacman.controller.pacman.IntroState;
 import de.amr.games.pacman.lib.TimedSeq;
 import de.amr.games.pacman.lib.V2d;
