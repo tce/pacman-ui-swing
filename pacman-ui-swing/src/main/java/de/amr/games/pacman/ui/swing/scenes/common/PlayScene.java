dfec3adcd446267d308f6a393fffa66d9dd43368
Armin Reichert <armin.reichert@web.de>
2022-12-10 07:35:27 +0100

    refac

--- pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/scenes/common/PlayScene.java
+++ pacman-ui-swing/src/main/java/de/amr/games/pacman/ui/swing/scenes/common/PlayScene.java
@@ -94,12 +94,12 @@ public class PlayScene extends GameScene {
 			} else {
 				r2D.drawFullMaze(g, r2D.mazeNumber(game.level().number()), 0, t(3));
 				r2D.drawDarkTiles(g, game.level().world().tiles(), tile -> game.level().world().containsEatenFood(tile)
-						|| game.level().world().isEnergizerTile(tile) && !game.energizerPulse.frame());
+						|| game.level().world().isEnergizerTile(tile) && !arcadeWorld.energizerPulse.frame());
 			}
 		} else {
 			r2D.drawFullMaze(g, r2D.mazeNumber(game.level().number()), 0, t(3));
-			r2D.drawDarkTiles(g, game.level().world().tiles(), tile -> game.level().world().containsEatenFood(tile)
-					|| game.level().world().isEnergizerTile(tile) && !game.energizerPulse.frame());
+			r2D.drawDarkTiles(g, game.level().world().tiles(),
+					tile -> game.level().world().containsEatenFood(tile) || game.level().world().isEnergizerTile(tile));
 		}
 		if (PacManGameUI.isDebugDraw()) {
 			DebugDraw.drawMazeStructure(g, game.level().world());
